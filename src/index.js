8e69c5aceab7a2ea4a75c9fa6ed61c2a6a697f87
mac1228 <68986081+alissa1228@users.noreply.github.com>
2022-04-23 17:30:40 +0900

    first commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React,{ StrictMode }  from 'react';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Amplify} from 'aws-amplify';
+import config from './aws-exports';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+Amplify.configure(config);
+
+ReactDOM.render(
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
