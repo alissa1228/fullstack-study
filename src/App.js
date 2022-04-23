fd8a011c2ef021110c3ef747cf29a538e6e2f126
mac1228 <68986081+alissa1228@users.noreply.github.com>
2022-04-23 17:43:06 +0900

    [add] ui-react css 추가

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import './App.css';
 
-import { withAuthenticator } from '@aws-amplify/ui-react';
+import { withAuthenticator} from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css'
 
 const App = () =>{
   return (
     <div>
       <h1>Hello World!</h1>
-
     </div>
   );
 }
