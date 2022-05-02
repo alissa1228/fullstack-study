e83fffb4807b106cca54017bd4da4e1de0b8ec78
mac1228 <68986081+alissa1228@users.noreply.github.com>
2022-05-03 00:54:11 +0900

    생성한 api 가져오기

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
-import React from 'react'
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { API } from "aws-amplify";
+import "@aws-amplify/ui-react/styles.css";
 
-import { withAuthenticator} from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css'
+const App = () => {
+  const [coins, updateCoins] = useState([]);
 
-const App = () =>{
+  //API 호출을 위한 함수 정의
+
+  const fetchCoins = async () => {
+    const data = await API.get("apife7d2e47", "/coins");
+    updateCoins(data.coins);
+  };
+
+  //컴포넌트가 마운트 될 떄 함수 호출
+  useEffect(() => {
+    fetchCoins();
+  }, []);
+
   return (
     <div>
       <h1>Hello World!</h1>
+      {coins.map((coin, index) => (
+        <div key={index}>
+          <h2>
+            {coin.name}- {coin.symbol}
+          </h2>
+          <h5>${coin.price_usd}</h5>
+        </div>
+      ))}
     </div>
   );
-}
+};
 
-export default withAuthenticator(App);
+export default App;
