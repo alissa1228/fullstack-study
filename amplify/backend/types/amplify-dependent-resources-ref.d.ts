16614e54507e259f070061732313c057518c0226
mac1228 <68986081+alissa1228@users.noreply.github.com>
2022-05-03 23:15:23 +0900

    #2 GraphQL API 설치

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,28 +1,9 @@
 export type AmplifyDependentResourcesAttributes = {
-    "auth": {
-        "project855159fa": {
-            "IdentityPoolId": "string",
-            "IdentityPoolName": "string",
-            "UserPoolId": "string",
-            "UserPoolArn": "string",
-            "UserPoolName": "string",
-            "AppClientIDWeb": "string",
-            "AppClientID": "string"
-        }
-    },
-    "function": {
-        "project54fb6be4": {
-            "Name": "string",
-            "Arn": "string",
-            "Region": "string",
-            "LambdaExecutionRole": "string"
-        }
-    },
     "api": {
-        "apife7d2e47": {
-            "RootUrl": "string",
-            "ApiName": "string",
-            "ApiId": "string"
+        "noteAPI": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
         }
     }
 }
